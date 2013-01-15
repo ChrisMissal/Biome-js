12e6386303222070f4feeb935ed484a6c77dcf34
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2013-01-14 23:31:36 -0600

    added simple wall collision so atoms bounce off edges

--- Biome/atom.js
+++ Biome/atom.js
@@ -1,17 +1,18 @@
 function randomAtom(radius, maxHeight, maxWidth)
 {
-    var speed = Math.floor(Math.random() * 2)
+    var velocityX = Math.floor(Math.random() * 4)
+    var velocityY = Math.floor(Math.random() * 4)
     var positionX = Math.floor(Math.random() * maxWidth);
     var positionY = Math.floor(Math.random() * maxHeight);
 
-    return new atom([positionX, positionY], speed, radius);
+    return new atom([positionX, positionY], [velocityX,velocityY], radius);
 }
 
-function atom(position, speed, radius)
+function atom(position, velocity, radius)
 {
     this.position = position;
     this.radius = radius;
-    this.speed = speed;
+    this.velocity = velocity;
     this.draw = function() {
         context.beginPath();
         context.fillStyle = 'green';
@@ -21,7 +22,24 @@ function atom(position, speed, radius)
         context.fill();
     };
     this.update = function() {
-        this.position[0] += this.speed;
-        this.position[1] += this.speed;
+        this.position[0] += this.velocity[0];
+        this.position[1] += this.velocity[1];
+
+        this.velocity = bounceOffOfEdges(this.radius, this.position, this.velocity);
     }
+}
+
+function bounceOffOfEdges(radius, position, velocity)
+{
+    var top = 0 + radius;
+    var bottom = WORLD_HEIGHT - radius;
+    var left = 0 + radius;
+    var right = WORLD_WIDTH - radius;
+
+    if(position[1] <= top || position[1] >= bottom)
+        velocity[1] *= -1;
+    if(position[0] <= left || position[0] >= right)
+        velocity[0] *= -1;
+
+    return velocity;
 }
